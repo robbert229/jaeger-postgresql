947140b5888e8719dfd3d3e4c3af7833b15435ad
John Rowley <johnrowleyster@gmail.com>
2024-04-04 09:50:08 -0700

    feat: added extra configuration methods (#40)

--- cmd/jaeger-postgresql-cleaner/main.go
+++ cmd/jaeger-postgresql-cleaner/main.go
@@ -2,41 +2,33 @@ package main
 
 import (
 	"context"
-	"flag"
 	"fmt"
+	"io/fs"
 	"log/slog"
+	"strings"
 	"time"
 
 	"github.com/jackc/pgx/v5/pgtype"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/robbert229/jaeger-postgresql/internal/logger"
 	"github.com/robbert229/jaeger-postgresql/internal/sql"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
 	"go.uber.org/fx"
 	"go.uber.org/fx/fxevent"
 )
 
-var (
-	databaseURLFlag      = flag.String("database.url", "", "the postgres connection url to use to connect to the database")
-	databaseMaxConnsFlag = flag.Int("database.max-conns", 20, "Max number of database connections of which the plugin will try to maintain at any given time")
-	loglevelFlag         = flag.String("log-level", "warn", "Minimal allowed log level")
-	maxSpanAgeFlag       = flag.Duration("max-span-age", time.Hour*24, "Maximum age of a span before it will be cleaned")
-)
-
 // ProvideLogger returns a function that provides a logger
 func ProvideLogger() any {
-	return func() (*slog.Logger, error) {
-		return logger.New(loglevelFlag)
+	return func(cfg Config) (*slog.Logger, error) {
+		return logger.New(&cfg.LogLevel)
 	}
 }
 
 // ProvidePgxPool returns a function that provides a pgx pool
 func ProvidePgxPool() any {
-	return func(logger *slog.Logger, lc fx.Lifecycle) (*pgxpool.Pool, error) {
-		if databaseURLFlag == nil {
-			return nil, fmt.Errorf("invalid database url")
-		}
-
-		databaseURL := *databaseURLFlag
+	return func(cfg Config, logger *slog.Logger, lc fx.Lifecycle) (*pgxpool.Pool, error) {
+		databaseURL := cfg.Database.URL
 		if databaseURL == "" {
 			return nil, fmt.Errorf("invalid database url")
 		}
@@ -54,10 +46,10 @@ func ProvidePgxPool() any {
 		// handle max conns
 		{
 			var maxConns int32
-			if databaseMaxConnsFlag == nil {
+			if cfg.Database.MaxConns == 0 {
 				maxConns = 20
 			} else {
-				maxConns = int32(*databaseMaxConnsFlag)
+				maxConns = int32(cfg.Database.MaxConns)
 			}
 
 			pgxconfig.MaxConns = maxConns
@@ -89,9 +81,9 @@ func ProvidePgxPool() any {
 }
 
 // clean purges the old roles from the database
-func clean(ctx context.Context, pool *pgxpool.Pool) (int64, error) {
+func clean(ctx context.Context, pool *pgxpool.Pool, maxAge time.Duration) (int64, error) {
 	q := sql.New(pool)
-	result, err := q.CleanSpans(ctx, pgtype.Timestamp{Time: time.Now().Add(-1 * *maxSpanAgeFlag), Valid: true})
+	result, err := q.CleanSpans(ctx, pgtype.Timestamp{Time: time.Now().Add(-1 * maxAge), Valid: true})
 	if err != nil {
 		return 0, err
 	}
@@ -99,23 +91,69 @@ func clean(ctx context.Context, pool *pgxpool.Pool) (int64, error) {
 	return result, nil
 }
 
-func main() {
-	flag.Parse()
+type Config struct {
+	Database struct {
+		URL      string `mapstructure:"url"`
+		MaxConns int    `mapstructure:"max-conns"`
+	} `mapstructure:"database"`
 
+	LogLevel string `mapstructure:"log-level"`
+
+	MaxSpanAge time.Duration `mapstructure:"max-span-age"`
+}
+
+func ProvideConfig() func() (Config, error) {
+	return func() (Config, error) {
+		pflag.String("database.url", "", "the postgres connection url to use to connect to the database")
+		pflag.Int("database.max-conns", 20, "Max number of database connections of which the plugin will try to maintain at any given time")
+		pflag.String("log-level", "warn", "Minimal allowed log level")
+		pflag.Duration("max-span-age", time.Hour*24, "Maximum age of a span before it will be cleaned")
+
+		v := viper.New()
+		v.SetEnvPrefix("JAEGER_POSTGRESQL")
+		v.AutomaticEnv()
+		v.SetConfigFile("jaeger-postgresql")
+		v.SetConfigType("yaml")
+		v.AddConfigPath(".")
+		v.SetEnvKeyReplacer(strings.NewReplacer(".", "_", "-", "_"))
+		pflag.Parse()
+		v.BindPFlags(pflag.CommandLine)
+
+		var cfg Config
+		if err := v.ReadInConfig(); err != nil {
+			_, ok := err.(*fs.PathError)
+			_, ok2 := err.(viper.ConfigFileNotFoundError)
+
+			if !ok && !ok2 {
+				return cfg, fmt.Errorf("failed to read in config: %w", err)
+			}
+		}
+
+		err := v.Unmarshal(&cfg)
+		if err != nil {
+			return cfg, fmt.Errorf("failed to decode configuration: %w", err)
+		}
+
+		return cfg, nil
+	}
+}
+
+func main() {
 	fx.New(
 		fx.WithLogger(func(logger *slog.Logger) fxevent.Logger {
 			return &fxevent.SlogLogger{Logger: logger.With("component", "uber/fx")}
 		}),
 		fx.Provide(
+			ProvideConfig(),
 			ProvideLogger(),
 			ProvidePgxPool(),
 		),
-		fx.Invoke(func(pool *pgxpool.Pool, lc fx.Lifecycle, logger *slog.Logger, stopper fx.Shutdowner) error {
+		fx.Invoke(func(cfg Config, pool *pgxpool.Pool, lc fx.Lifecycle, logger *slog.Logger, stopper fx.Shutdowner) error {
 			go func(ctx context.Context) {
 				ctx, cancelFn := context.WithTimeout(ctx, time.Minute)
 				defer cancelFn()
 
-				count, err := clean(ctx, pool)
+				count, err := clean(ctx, pool, cfg.MaxSpanAge)
 				if err != nil {
 					logger.Error("failed to clean database", "err", err)
 					stopper.Shutdown(fx.ExitCode(1))
--- go.mod
+++ go.mod
@@ -33,6 +33,7 @@ require (
 	github.com/docker/go-units v0.5.0 // indirect
 	github.com/fatih/color v1.16.0 // indirect
 	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/go-logr/logr v1.4.1 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-ole/go-ole v1.3.0 // indirect
@@ -41,6 +42,7 @@ require (
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/hashicorp/go-hclog v1.6.2 // indirect
 	github.com/hashicorp/go-plugin v1.6.0 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/hashicorp/yamux v0.1.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20231201235250-de7065d80cb9 // indirect
@@ -54,6 +56,7 @@ require (
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mfridman/interpolate v0.0.2 // indirect
 	github.com/mitchellh/go-testing-interface v1.14.1 // indirect
+	github.com/mitchellh/mapstructure v1.5.1-0.20231216201459-8508981c8b6c // indirect
 	github.com/moby/docker-image-spec v1.3.1 // indirect
 	github.com/moby/patternmatcher v0.6.0 // indirect
 	github.com/moby/sys/sequential v0.5.0 // indirect
@@ -63,6 +66,7 @@ require (
 	github.com/oklog/run v1.1.0 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/opencontainers/image-spec v1.1.0 // indirect
+	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/power-devops/perfstat v0.0.0-20240221224432-82ca36839d55 // indirect
@@ -70,10 +74,18 @@ require (
 	github.com/prometheus/common v0.51.1 // indirect
 	github.com/prometheus/procfs v0.13.0 // indirect
 	github.com/rogpeppe/go-internal v1.12.0 // indirect
+	github.com/sagikazarmark/locafero v0.4.0 // indirect
+	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
 	github.com/sethvargo/go-retry v0.2.4 // indirect
 	github.com/shirou/gopsutil/v3 v3.24.2 // indirect
 	github.com/shoenig/go-m1cpu v0.1.6 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/sourcegraph/conc v0.3.0 // indirect
+	github.com/spf13/afero v1.11.0 // indirect
+	github.com/spf13/cast v1.6.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/spf13/viper v1.18.2 // indirect
+	github.com/subosito/gotenv v1.6.0 // indirect
 	github.com/tklauser/go-sysconf v0.3.13 // indirect
 	github.com/tklauser/numcpus v0.7.0 // indirect
 	github.com/yusufpapurcu/wmi v1.2.4 // indirect
@@ -93,5 +105,6 @@ require (
 	golang.org/x/tools v0.19.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240325203815-454cdb8f5daa // indirect
 	google.golang.org/protobuf v1.33.0 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
